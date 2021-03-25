c1bfbf66046663fe139f71a5f28bf452f96ddc55
Goutam Borthakur <59662170+goutamborthakur555@users.noreply.github.com>
2021-03-25 18:18:27 +0530

    Update MommyPoppinsScraper_GB.py

--- MommyPoppinsScraper_GB.py
+++ MommyPoppinsScraper_GB.py
@@ -8,12 +8,13 @@ os.chdir("....\\Web Scraping\\Mommy Poppins") # changing the directory
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import re
+
+# Links from where data need to be extracted/scrapped
+master_link = 'https://mommypoppins.com/events/115/los-angeles/all/tag/all/age/03-01-2021/03-31-2021/deals/0/near/0/0' #Enter the website link
 
 # Opening the chrome browser using webdriver
 driver = webdriver.Chrome(executable_path=r"chromedriver_win32\chromedriver.exe")
-
-master_link = 'https://mommypoppins.com/events/115/los-angeles/all/tag/all/age/03-01-2021/03-31-2021/deals/0/near/0/0' #Enter the website link
-
 driver.get(master_link)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 mains = soup.find_all("a", {"angularticsaction": "expanded-detail"})
@@ -35,15 +36,19 @@ for main in attributes:
         except:
             mp_link.append("")
 
-driver.close()
+driver.quit() #To close all the tab
+#driver.close() #To close the current tab
 
 #Empty list for remaining items
 date = []
 evnt_time = []
 age = []
 price = []
-website = []
 description = []
+phone = []
+email = []
+website = []
+ind_attributes_lst = []
 
 # Opening the chrome browser using webdriver
 driver = webdriver.Chrome(executable_path=r"chromedriver_win32\chromedriver.exe")
@@ -55,9 +60,10 @@ for ind_lnk in mp_link:
     #see_all_dates = driver.find_elements_by_xpath('//*[@id="node-state"]/div[1]/div/app-eventinfo/div/div[1]/span/a')
     #see_all_dates[0].click()
     
+    #date.append(driver.find_element_by_css_selector("#node-state > div.content-head-wrapper.content-wrapper.event.ng-star-inserted > div > app-eventinfo > div > div.date-repeats.ng-star-inserted").text)
+    
     try:
         date.append(driver.find_element_by_css_selector("#node-state > div.content-head-wrapper.content-wrapper.event > div > app-eventinfo > div > div.date-repeats > strong").text)
-        #date.append(driver.find_element_by_css_selector("#node-state > div.content-head-wrapper.content-wrapper.event.ng-star-inserted > div > app-eventinfo > div > div.date-repeats.ng-star-inserted").text)
     except:
         date.append("")
     try:
@@ -72,35 +78,51 @@ for ind_lnk in mp_link:
         price.append(driver.find_element_by_css_selector("#node-state > div.content-head-wrapper.content-wrapper.event.ng-star-inserted > div > app-eventinfo > div > div:nth-child(4) > span").text)
     except:
         price.append("")
-    
-    # To extract the Website link
-    ind_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    ind_mains = ind_soup.find_all("a", {"angularticsaction": "contentClick"})
-    ind_attributes = [{el.text: el.get('href')} for el in ind_mains]
-    
-    for web_lnk in ind_attributes:
-        for k,v in web_lnk.items():
-            try:
-                website.append(v)
-            except:
-                website.append("")
     try:
         description.append(driver.find_element_by_css_selector("#node-state > div.content-head-wrapper.content-wrapper.event.ng-star-inserted > div > app-bodyparsed").text)
     except:
         description.append("")
+    
+    # To extract the list for Website links
+    ind_soup = BeautifulSoup(driver.page_source, 'html.parser')
+    ind_mains = ind_soup.find_all("a", {"angularticsaction": "contentClick"})
+    ind_attributes = [{el.text: el.get('href')} for el in ind_mains]
+    ind_attributes_lst.append(ind_attributes)
 
-driver.close()
+driver.quit() #To close all the tab
+#driver.close() #To close the current tab
+
+# Filtering out the phone number
+for ph_attr in ind_attributes_lst:
+    ph_check = []
+    for ph_dct in ph_attr:
+        ph_check += [val for val in ph_dct.values() if re.match(r'tel:\(?(\d{3})\)?[^\d]*(\d{3})[^\d]*(\d{4})', val)]
+    phone.append(', '.join(ph_check)[4:].replace('.', ' ') if ph_check else '')
+
+# Filtering out the email id
+for em_attr in ind_attributes_lst:
+    em_check = []
+    for em_dct in em_attr:
+        em_check += [val for val in em_dct.values() if re.match(r'mailto:([\w\.]+@\w+\.\w+).*', val)]
+    email.append(', '.join(em_check)[7:].split('?')[0] if em_check else '')
+
+# Filtering up the website links
+for web_attr in ind_attributes_lst:
+    vl_check = []
+    for dct in web_attr:
+        vl_check += [val for val in dct.values() if re.match(r'https?[/\w\.]*', val)]
+    website.append(', '.join(vl_check) if vl_check else '')
 
 # Save work in excel file
 data = {}
-data = {'Name': name, 'MommyPoppins_Link': mp_link, 'Date': date, 'Time': evnt_time, 'Age': age, 'Price' : price, 'Website' : website, 'Description' : description}
+data = {'Name': name, 'MommyPoppins_Link': mp_link, 'Date': date, 'Time': evnt_time, 'Age': age, 'Price' : price, 'Phone' : phone, 'Email' : email,'Website' : website, 'Description' : description}
 
 # Converting to dataframe
 result = pd.DataFrame(dict([(k,pd.Series(v)) for k,v in data.items()])) #As all arrays are of not same lenght 
-header = ['Name', 'MommyPoppins_Link', 'Date', 'Time', 'Age', 'Price', 'Website', 'Description']
+header = ['Name', 'MommyPoppins_Link', 'Date', 'Time', 'Age', 'Price', 'Phone', 'Email', 'Website', 'Description']
 
 # Exporting to .xlsx file
-result.to_excel("MommyPoppins_50.xlsx", columns = header, index = False)
+result.to_excel("MommyPoppins_50_sample_records.xlsx", columns = header, index = False)
 
 ########################################################################
 
